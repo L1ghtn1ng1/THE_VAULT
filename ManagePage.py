1245d2930e13cc982c87fb8a7acc50e18ff28e20
L1ghtn1ng1 <77595914+L1ghtn1ng1@users.noreply.github.com>
2022-07-30 14:44:12 +1200

    Updated Manage page to show all saved passwords
    Page only displayed pre written passwords, fixed now with a for loop that produces an updated list of passwords every time. Stores passwords in a text file - PasswordsList.txt

--- ManagePage.py
+++ ManagePage.py
@@ -3,34 +3,42 @@ from tkinter import ttk
 
 class page():
     def create(self):
+        self.pswdBtnList = []  # Create empty list for all password names
+        self.passwords = []  # Create empty list for all passwords.
+        try:
+            filething = open("PasswordsList.txt", "r")  # Open and read file.
+            for line in filething:
+                lineList = line.split(":")  # Split the page into two columns with ':'.
+                self.pswdBtnList.append(lineList[0])  # Appends the first column (the names) to the empty list
+                # dedicated for it.
+                self.passwords.append(lineList[1])  # Appends the second column (the passwords) to the empty list
+                # dedicated for it.
+            filething.close()  # Close the file.
+        except:
+            print("No passwords found yet")  # If no passwords are saved, there will be nothing in the file,
+            # hence, print this.
 
+        # Label the title.
         self.Title = ttk.Label(self,
-                               text = "Manage Passwords",
-                               font = ("Video Cond", 25))
-        self.Title.grid(column = 1, row = 1)
+                               text="Manage Passwords",
+                               font=("Video Cond", 25))
+        self.Title.grid(column=1, row=0)  # Place on grid into window.
 
-        self.details = ttk.Button(self,
-                                  text = "Password 1")
-        self.details.grid(column = 1, row = 2)
-
-        self.details = ttk.Button(self,
-                                  text="Password 2")
-        self.details.grid(column=1, row=3)
-
-        self.details = ttk.Button(self,
-                                  text="Password 3")
-        self.details.grid(column=1, row=4)
-
-        self.details = ttk.Button(self,
-                                  text="Password 4")
-        self.details.grid(column=1, row=5)
+        # The for loop makes as many buttons as there are passwords. All have the same command, to display the
+        # password corresponding to the password name. This produces an updated list of passwords to view everytime a
+        # password is added.
+        for element in self.pswdBtnList:
+            self.details = ttk.Button(self,
+                                      text=element,
+                                      command=lambda tempz=element: self.showsavedPass(tempz))
+            self.details.grid(column=1, row=self.pswdBtnList.index(element) + 1)  # Place on grid into window.
 
         self.details = ttk.Button(self,
                                   text="Add Password",
-                                  command=lambda: self.changePage(3))
-        self.details.grid(column=1, row=6)
+                                  command=lambda: self.changePage(3))  # Switches to add password page.
+        self.details.grid(column=1, row=998)  # Places it at second to last in the window.
 
         self.goBack = ttk.Button(
             self,
             text="Cancel", command=lambda: self.changePage(1))
-        self.goBack.grid(column=1, row=7)
+        self.goBack.grid(column=1, row=999)  # Places it at the very last in the window.
