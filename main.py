3184725d5e00e9d89a8c3fb8dd38913b21dc421b
L1ghtn1ng1 <77595914+L1ghtn1ng1@users.noreply.github.com>
2022-07-30 12:00:06 +1200

    Code to set up pages and change page function.
    This is the base code that prepares for the pages I will create and a function that will allow me to change from one page to another.

---
+++ main.py
@@ -0,0 +1,40 @@
+# Base libraries
+import tkinter as tk
+from tkinter import ttk
+
+
+class App(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self)  # initialize the superclass(frame)
+
+        # Page list
+        # ADD NEW CLASSES YOU MAKE TO LIST!  (pages will be indexed chronologically)
+        self.availablePages = [
+
+        ]
+        #
+        #
+        # Split into 3 columns
+        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(index=1, weight=1)
+        self.columnconfigure(index=2, weight=1)
+
+        # Show first page on list at start up
+        self.availablePages[0].create(self)
+
+    # Function to change between pages
+    def changePage(self, number):
+        for widget in self.winfo_children():  # For each widget on screen
+            widget.destroy()  # Destroy each widget found
+            #
+            #
+            # Run the create function on the desired page
+            self.availablePages[number].create(self)
+
+
+if __name__ == "__main__":  # If this file is run directly, run the following code
+    root = tk.Tk()  # Create a window
+    root.title("The Vault")  # Add title
+    app = App(root)  # Link the App and window we made
+    app.pack(fill="both", expand=True)  # Allow resizing
+    root.mainloop()  # Run the app
